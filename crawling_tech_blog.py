719d91ac9003cfe48390b5103356492954df7530
moduaiffel <moduaiffel@gmail.com>
2021-05-06 23:19:05 +0900

    add line baemin

--- crawling_tech_blog.py
+++ crawling_tech_blog.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def parsing_beautifulsoup(url):
     data = requests.get(url)
 
@@ -8,21 +9,22 @@ def parsing_beautifulsoup(url):
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-# def extract_blog_naver(soup):
-#     upload_contents = ''
-#     new_posts_naver = soup.select(".post_article")
-#     url_prefix_naver = "https://d2.naver.com/home"
+def extract_blog_naver(soup):
 
-#     for new_post in new_posts_naver:
-#         blog_title = new_post.select("a")[0].text
-#         url_suffix = new_post.select("a")[1].attrs['href']        
-#         created_date = new_post.select("dd")[0].text
-#         url = url_prefix_naver + url_suffix
+    upload_contents = ''
+    new_posts = soup.select(".pc > .post_article")
+    url_prefix_naver = "https://d2.naver.com/home"
 
-#         content = f"<a href={url}" + blog_title + "</a>" + ", " + created_date + "<br/>\n"
-#         upload_contents += content
+    for new_post in new_posts:
+        blog_title = new_post.select("a")[0].text
+        url_suffix = new_post.select("a")[1].attrs['href']        
+        created_date = new_post.select("dd")[0].text
+        url = url_prefix_naver + url_suffix
 
-#     return upload_contents
+        content = f"<a href={url}" + blog_title + "</a>" + ", " + created_date + "<br/>\n"
+        upload_contents += content
+
+    return upload_contents
 
 def extract_blog_kakao(soup):
     upload_contents = ''
@@ -42,5 +44,37 @@ def extract_blog_kakao(soup):
     return upload_contents    
 
 
+def extract_blog_line(soup):
+    upload_contents = ''
+    new_posts = soup.select(".ast-row > article")
+    # url_prefix_line = "https://engineering.linecorp.com/ko/blog/"
+
+    for new_post in new_posts:
+
+        blog_title = new_post.select('h2')[0].text
+        created_date = new_post.select_one('.published').text
+        url_suffix = new_post.select('a')[0].attrs['href']
+        url = url_suffix
+
+        content = f"<a href={url}>" + blog_title + "</a>" + " " + created_date + "<br/>\n"
+        upload_contents += content
+
+    return upload_contents   
 
 
+def extract_blog_baemin(soup):
+    upload_contents = ''
+    new_posts = soup.select(".list > .list-module")
+    url_prefix_baemin = "https://woowabros.github.io/"
+
+    for new_post in new_posts[:5]:
+
+        blog_title = new_post.select('h2')[0].text
+        created_date = new_post.select('span')[0].text  
+        url_suffix = new_post.select('a')[0].attrs['href']
+        url = url_prefix_baemin + url_suffix
+
+        content = f"<a href={url}>" + blog_title + "</a>" + " " + created_date + "<br/>\n"
+        upload_contents += content
+
+    return upload_contents   
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from pytz import timezone
-from crawling_tech_blog import parsing_beautifulsoup, extract_blog_kakao
+from crawling_tech_blog import parsing_beautifulsoup, extract_blog_naver, extract_blog_kakao, extract_blog_line, extract_blog_baemin
 from github_utils import get_github_repo, upload_github_issue
 
 if __name__ == "__main__":
@@ -15,16 +15,22 @@ if __name__ == "__main__":
     today_data = today.strftime("%Y년 %m월 %d일")
 
     # naver_blog_url = "https://d2.naver.com/home"
-    kakao_blog_url = "https://tech.kakao.com/blog/"
-    # line_blog_url = "https://engineering.linecorp.com/ko/blog/"
+    blog_url_kakao = "https://tech.kakao.com/blog/"
+    blog_url_line = "https://engineering.linecorp.com/ko/blog/"
+    blog_url_baemin = "https://woowabros.github.io/"
 
     # soup = parsing_beautifulsoup(naver_blog_url)
-    soup = parsing_beautifulsoup(kakao_blog_url)
-    # soup = parsing_beautifulsoup(line_blog_url)
+    soup_kakao = parsing_beautifulsoup(blog_url_kakao)
+    soup_line = parsing_beautifulsoup(blog_url_line)
+    soup_baemin = parsing_beautifulsoup(blog_url_baemin)
+
     
-    issue_title = f"네카라쿠배 기술블로그 새글알림({today_data}"
+    issue_title = f"네카라쿠배 기술블로그 새글알림({today_data})"
+    upload_contents = ''
     # upload_contents = extract_blog_naver(soup)
-    upload_contents = extract_blog_kakao(soup)
+    upload_contents += extract_blog_kakao(soup_kakao)
+    upload_contents += extract_blog_line(soup_line)
+    upload_contents += extract_blog_baemin(soup_baemin)
 
     repo = get_github_repo(access_token, repository_name)
     upload_github_issue(repo, issue_title, upload_contents)
